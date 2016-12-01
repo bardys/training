ec955349804122258e92deeb56884403b60d1d8f
Andrii Bardys <anbar@kyiv.ciklum.net>
2016-12-01 16:10:13 +0200

    test on google

---
+++ abardys/src/test/java/com/ciklum/test/Test1.java
@@ -0,0 +1,30 @@
+package com.ciklum.test;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by anbar on 23-Nov-16.
+ */
+public class Test1 {
+
+   /* @Test
+    public void openGoogle() throws InterruptedException {
+
+        //System.setProperty("webdriver.chrome.driver", "C:/Users/anbar/Desktop/training/git/training/abardys/geckodriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+       / driver.get("http://www.google.com");
+       // Thread.sleep(5000);
+        driver.close();
+
+    }*/
+
+   @Test
+    public void openGoogle(){
+       WebDriver driver = new ChromeDriver();
+       driver.get("https://www.google.com.ua");
+       driver.quit();
+   }
+}
